6cd0d31979f9d9028e1a6dcc32de8a904e8cd422
Inamulla01 <mohamedrahmatullahinamullah@gmail.com>
2025-09-10 09:06:02 +0530

    update

---
+++ src/lk/com/pos/util/AppIconUtil.java
@@ -0,0 +1,31 @@
+
+package lk.com.pos.util;
+
+import java.awt.Image;
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class AppIconUtil {
+    
+    private static Image appIcon;
+    
+    static {
+        try {
+            URL iconPath = AppIconUtil.class.getResource("/lk/com/dfw/img/logo.png");
+            ImageIcon icon = new ImageIcon(iconPath);
+            AppIconUtil.appIcon = icon.getImage();
+        } catch (NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Invalid icon path...");
+        }
+    }
+
+  
+
+    public static void applyIcon(JFrame frame) {
+        if (frame != null) {
+            frame.setIconImage(appIcon);
+        }
+    }
+}

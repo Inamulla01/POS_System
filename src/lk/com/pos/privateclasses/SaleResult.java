2067d2446c5385be48e45b193d17baf2c9e24829
inaamul09 <contact.inaamulhassan@gmail.com>
2025-11-25 10:30:54 +0530

    Jasper Report added, and print report functionality still developing

---
+++ src/lk/com/pos/privateclasses/SaleResult.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.com.pos.privateclasses;
+
+/**
+ *
+ * @author User
+ */
+public class SaleResult {
+    public final int salesId;
+    public final String invoiceNo;
+
+    public SaleResult(int salesId, String invoiceNo) {
+        this.salesId = salesId;
+        this.invoiceNo = invoiceNo;
+    }
+}
